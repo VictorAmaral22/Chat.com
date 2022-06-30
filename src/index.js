7072a75f820a7282ed821b397ca22a357d329a2f
Victor Amaral <victortavamaral@gmail.com>
2022-06-30 16:24:48 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,10 +5,39 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+// SOCKET
+const {
+    Server
+} = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.client.nick = 'Unknown'+Math.floor(Math.random() * 100);
+    io.emit('chat message', socket.client.nick + ' entrou no sala de chat ');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('chat message', socket.client.nick + ' deixou sala de chat ');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log(socket.client.id + ': ' + msg);
+        io.emit('chat message', socket.client.nick + ': ' + msg);
+    });
+
+    socket.on('set nick', (msg) => {
+        const oldNick = socket.client.nick;
+        io.emit('chat message', `${oldNick} trocou seu nome para ${msg}`);
+        socket.client.nick = msg;
+    });
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
 server.listen(3000, () => {
-  console.log('listening on http://localhost:3000');
+    console.log('listening on http://localhost:3000');
 });

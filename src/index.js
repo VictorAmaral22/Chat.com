5a06e99a0a5cd07c193df89fb9322dd660dca0b9
Victor Amaral <victortavamaral@gmail.com>
2022-06-30 17:37:46 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -10,20 +10,43 @@ const {
     Server
 } = require("socket.io");
 const io = new Server(server);
+var usersTyping = [];
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.client.nick = 'Unknown'+Math.floor(Math.random() * 100);
     io.emit('chat message', socket.client.nick + ' entrou no sala de chat ');
 
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        usersTyping = usersTyping.filter(item => item.id != socket.client.id);
         io.emit('chat message', socket.client.nick + ' deixou sala de chat ');
+    });
+
+    socket.on('user typing', () => {
+        if(!usersTyping.find(item => item.id == socket.client.id)){
+            console.log('socket.client.nick ',socket.client.nick)
+            usersTyping.push({ id: socket.client.id, nick: socket.client.nick});
+            console.log('usersTyping ',usersTyping)
+            // let typersExceptU = usersTyping.filter(item => item.id != socket.client.id)
+            io.emit('users typing list', usersTyping);
+        }
+    });
+
+    socket.on('user stopped typing', () => {
+        console.log(socket.client.id + ' parou de digitar');
+        usersTyping = usersTyping.filter(item => item.id != socket.client.id);
+        console.log('usersTyping ',usersTyping)
+        // let typersExceptU = usersTyping.filter(item => item.id != socket.client.id)
+        io.emit('users typing list', usersTyping);
     });
 
     socket.on('chat message', (msg) => {
         console.log(socket.client.id + ': ' + msg);
+        usersTyping = usersTyping.filter(item => item.id != socket.client.id);
         io.emit('chat message', socket.client.nick + ': ' + msg);
+        io.emit('users typing list', usersTyping);
     });
 
     socket.on('set nick', (msg) => {
